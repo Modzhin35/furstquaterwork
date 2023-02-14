89dcdb9233ebb0838df56c93b10c0d5d2ede0623
modzhin <modzhds@gmail.com>
2023-02-14 23:31:33 +0300

    Add method FillUserArray

--- homework/Program.cs
+++ homework/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿string[] FillUserArray(string message, int size)
+{
+    string[] userArr = new string[size];
+for(int i=0; i<size; i++){
+        Console.WriteLine($"{message} {i + 1}:");
+        userArr[i] = Console.ReadLine();
+    }
+    return userArr;
+}
+
+
+int sizeOfWords=3;
+string[] userWordsArray = FillUserArray("Введите строку элемента массива №", 5);
